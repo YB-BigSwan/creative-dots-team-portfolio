0b11716b25c00a088368cbc0ab3f9071c721141c
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-11-05 19:48:03 +0200

    Content and Styling Updates:
    - Updates logo and favicon svgs
    - Updates page styling
    - Adds custom defined CSS colors for uniform styling
    - Adds Google font ‘quicksand’ for uniform styling
    - Updates some theme colors
    - Adds member info
    - Adds almost all LinkedIn/GitHub links
    - Nav styling updated
    
    In Progress:
    
    - Nav styling (Bootstrap difficulties)
    - Carousel background styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,30 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../App.css";
 
 function NavBar() {
   return (
     <>
-      <Navbar>
+      <Navbar className="navbar" expand="lg">
         <Container>
-          <Nav className="flex-column">
-            <Link to="/members" className="nav-link">
+          <Nav className="navbar-nav custom-nav">
+            <NavLink
+              to="/members"
+              className="nav-link"
+              activeClassName="active"
+            >
               Members
-            </Link>
-            <Link to="/projects" className="nav-link">
+            </NavLink>
+
+            <NavLink
+              to="/projects"
+              className="nav-link"
+              activeClassName="active"
+            >
               Projects
-            </Link>
+            </NavLink>
           </Nav>
         </Container>
       </Navbar>
--- src/components/TeamProject.jsx
+++ src/components/TeamProject.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
-import alpoCardImage from "../assets/member-photos/alpo-card-img.png";
 import Logo from "./Logo";
 import "../App.css";
 
@@ -21,28 +20,29 @@ function TeamProject() {
 
         <h2 className="tagline">Connecting the dots to success</h2>
       </header>
-      <h1>Our project</h1>
       <Carousel activeIndex={index} onSelect={handleSelect} interval={8000}>
-
         <Carousel.Item>
-          <img src="https://www.solidbackgrounds.com/images/3840x2160/3840x2160-dark-gray-solid-color-background.jpg" height={"500px"}/>
+          <img
+            src="https://www.solidbackgrounds.com/images/3840x2160/3840x2160-dark-gray-solid-color-background.jpg"
+            height={"500px"}
+          />
           <Carousel.Caption>
-          <h3>Design challenge</h3>
+            <h3 className="projectDescription">Design challenge</h3>
             <p className="projectDescription">
-              The design challenge for our project was introduced by the city of Helsinki,
-              as they are looking for ways to increase activeness among young people.  
-              <br/>
-              <br/>
+              The design challenge for our project was introduced by the city of
+              Helsinki, as they are looking for ways to increase activeness
+              among young people.
+              <br />
+              <br />
               Among the different themes provided, we chose:
-              <br/>
-              1. Social interaction as a promoter of physical activity among 
+              <br />
+              1. Social interaction as a promoter of physical activity among
               upper secondary students.
-              <br/>
+              <br />
               2. New solutions for more effectively balancing supply and demand.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
-
       </Carousel>
     </>
   );

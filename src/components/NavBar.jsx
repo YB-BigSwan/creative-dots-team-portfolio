413002737b2bc5ad0a41340e820fa475c16b6a5b
elinapekka <90378752+elinapekka@users.noreply.github.com>
2023-11-20 23:12:15 +0200

    dropdown nav for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
 import "../App.css";
 
@@ -8,33 +9,62 @@ function NavBar() {
   return (
     <>
       <Navbar className="navbar" expand="lg">
+        <div className="navbarWide">
         <Container>
-          <Nav className="navbar-nav custom-nav">
-            <NavLink
-              to="/members"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Members
-            </NavLink>
+            <Nav className="navbar-nav custom-nav">
+              <NavLink
+                to="/members"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Members
+              </NavLink>
 
-            <NavLink
-              to="/projects"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Projects
-            </NavLink>
-            <NavLink
-              to="/storyboard"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Storyboard
-            </NavLink>
-          </Nav>
+              <NavLink
+                to="/projects"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Projects
+              </NavLink>
+              <NavLink
+                to="/storyboard"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Storyboard
+              </NavLink>
+            </Nav>
         </Container>
+        </div>
+      
+        <NavDropdown title="Menu" className="navbar-nav custom-nav dropdown-nav">
+          <NavLink
+            to="/members"
+            className="nav-link"
+            activeClassName="active"
+          >
+            Members
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className="nav-link"
+            activeClassName="active"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/storyboard"
+            className="nav-link"
+            activeClassName="active"
+          >
+            Storyboard
+          </NavLink>
+        </NavDropdown>
+      
+
       </Navbar>
+
     </>
   );
 }

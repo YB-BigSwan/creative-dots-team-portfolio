e306c6ee385d7daa24bc23fda06cd6cd88effb2b
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-12-05 22:45:09 +0200

    adds prototype v1 and changes name of v2 to v2

--- src/components/Prototype1.jsx
+++ src/components/Prototype1.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import "../App.css";
 
-function Prototype() {
+function Prototype1() {
   return (
     <>
       <header>
@@ -17,11 +17,11 @@ function Prototype() {
         style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
         width="927px"
         height="600px"
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FXo1SmNAIzJqoVXA0GSvlKD%2FDSD_Prototype_V2%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DXtoKCcOZDJjUGnzt-1"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FC0GWUK5Pc9LV5qEawP8xYd%2FFitness-App-Prototype%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DLzcpAnu2tS67vizA-1"
         allowFullScreen
       ></iframe>
     </>
   );
 }
 
-export default Prototype;
+export default Prototype1;

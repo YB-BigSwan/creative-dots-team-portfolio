5c4e43345df1beac9f4c690ec6af5e5890020bb6
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-12-11 19:16:31 +0200

    adds usability testing results view

---
+++ src/components/UsabilityTesting.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
+import Logo from "./Logo";
+import template from "../assets/project-photos/template.png";
+import "../App.css";
+
+function TeamProject() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <>
+      <header>
+        <Link to="/" className="logo-link">
+          <Logo />
+        </Link>
+
+        <h2 className="tagline">Connecting the dots to success</h2>
+      </header>
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={3000}>
+        <Carousel.Item>
+          <img src={template} />
+          <Carousel.Caption>
+            <h3 className="projectDescription">Usability Testing</h3>
+            <div className="testingDescription">
+              Our usability testing was conducted over two days in the 6th floor
+              lab. We used a standardized evaluation form, as well as the
+              emotion tracking software to collect feedback to iterate upon.
+              <br />
+              <br />
+              Main Findings:
+              <br />
+              <ul>
+                <li>
+                  The main purpose of the app, event navigation, and finding
+                  worked well.
+                </li>
+                <li>
+                  Two filters at the same time were not implemented, and the
+                  functionality didn't work as intended.
+                </li>
+                <li>
+                  Home screen menu placement caused uncertainty for the user.
+                </li>
+                <li>
+                  Hamburger menu usage was disliked; preferred a fixed menu
+                  without the need for opening.
+                </li>
+                <li>Consistency in menu position was emphasized.</li>
+                <li>
+                  Map feedback: unclear content, disliked the shape (preferred a
+                  square).
+                </li>
+              </ul>
+              <br />
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+}
+
+export default TeamProject;
